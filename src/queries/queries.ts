0c6ba5e4216d25d0a49e5a0c982262916de16b97
Takumi <nase49946@gmail.com>
2023-02-04 22:14:35 +0900

    feat: 型自動生成

--- src/queries/queries.ts
+++ src/queries/queries.ts
@@ -9,7 +9,6 @@ export const GET_USERS = gql`
     }
   }
 `;
-
 export const GET_USERS_LOCAL = gql`
   query GetUsers {
     users(order_by: { created_at: desc }) @client {
@@ -19,24 +18,22 @@ export const GET_USERS_LOCAL = gql`
     }
   }
 `;
-
-export const GET_USER_IDS = gql`
-  query GetUsers {
+export const GET_USERIDS = gql`
+  query GetUserIds {
     users(order_by: { created_at: desc }) {
       id
     }
   }
 `;
-
-export const GET_USER_BY_ID = gql`
+export const GET_USERBY_ID = gql`
   query GetUserById($id: uuid!) {
     users_by_pk(id: $id) {
+      id
       name
       created_at
     }
   }
 `;
-
 export const CREATE_USER = gql`
   mutation CreateUser($name: String!) {
     insert_users_one(object: { name: $name }) {
@@ -46,7 +43,6 @@ export const CREATE_USER = gql`
     }
   }
 `;
-
 export const DELETE_USER = gql`
   mutation DeleteUser($id: uuid!) {
     delete_users_by_pk(id: $id) {
@@ -56,7 +52,6 @@ export const DELETE_USER = gql`
     }
   }
 `;
-
 export const UPDATE_USER = gql`
   mutation UpdateUser($id: uuid!, $name: String!) {
     update_users_by_pk(pk_columns: { id: $id }, _set: { name: $name }) {
