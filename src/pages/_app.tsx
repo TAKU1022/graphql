3616a158852933ade354b75f458760659d34a935
Takumi <nase49946@gmail.com>
2023-02-11 16:46:26 +0900

    feat: fetch users

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
+import { ApolloProvider } from '@apollo/client';
 import { AppProps } from 'next/app';
+import { initializeApollo } from '../lib/appolloClient';
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  const client = initializeApollo();
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 };
 
 export default MyApp;
---
+++ src/pages/hasura-main.tsx
@@ -0,0 +1,46 @@
+import { NextPage } from 'next';
+import Link from 'next/link';
+import { useQuery } from '@apollo/client';
+import { GET_USERS } from '../queries/queries';
+import { GetUsersQuery } from '../types/generated/graphql';
+import { Layout } from '../components/Layout';
+
+const FetchMain: NextPage = () => {
+  const { data, error, loading } = useQuery<GetUsersQuery>(GET_USERS);
+
+  console.log(data);
+
+  if (error) {
+    return (
+      <Layout title="Hasura fetchPolicy">
+        <p>Error: {error.message}</p>
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout title="Hasura fetchPolicy">
+      <p className="mb-6 font-bold">Hasura main page</p>
+
+      {loading ? (
+        <>Loading...</>
+      ) : (
+        <>
+          {data.users.map((user) => {
+            return (
+              <p className="my-1" key={user.id}>
+                {user.name}
+              </p>
+            );
+          })}
+        </>
+      )}
+
+      <Link href="/hasura-sub">
+        <a className="mt-6">Next</a>
+      </Link>
+    </Layout>
+  );
+};
+
+export default FetchMain;
